6bb28808afb13516a90d18c276ffe90693c5da24
Armando Ramos Sanchez <armandoramossanchenz@gmail.com>
2021-11-26 19:39:09 +0100

    arreglo errores de diseño

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
 var listasVideos = document.getElementsByClassName("lista-videos");
 
-// document.getElementById("zelda-wind-waker-h1").addEventListener("click", function(){
-//     abierto = !abierto;
-//     if (abierto) {
-//         document.getElementById("zelda-wind-waker-lista").style.transform = "scaleY(1)";
-//         document.getElementById("zelda-wind-waker-lista").style.maxHeight = "1000em";
-//     }else{
-//         document.getElementById("zelda-wind-waker-lista").style.transform = "scaleY(0)";
-//         document.getElementById("zelda-wind-waker-lista").style.maxHeight = "0px";
-//     }
-// }, false);
-
 const abrirListaVideos = (lista) =>{
-    console.log(document.getElementById(lista).classList);
     if (document.getElementById(lista).classList.contains("cerrada")) {
         for (let i = 0; i < listasVideos.length; i++) {
             listasVideos[i].classList.remove("abierta");
