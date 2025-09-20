1e0ed51578c9cab1ad44de5c25aa91033c284b6a
aramossanchez <armandoramossanchez@gmail.com>
2025-09-20 18:13:51 +0200

    fix: añadida url controlada para poder hacer funcionar links en pages

--- script.js
+++ script.js
@@ -1,42 +1,43 @@
 var listasVideos = document.getElementsByClassName("lista_videos");
 
 const abrirListaVideos = (lista) => {
-    if (document.getElementById(lista).classList.contains("cerrada")) {
-        for (let i = 0; i < listasVideos.length; i++) {
-            listasVideos[i].classList.remove("abierta");
-            listasVideos[i].classList.add("cerrada");
-            document.getElementById(lista).classList.remove("cerrada");
-            document.getElementById(lista).classList.add("abierta");
-        }
-        return;
+  if (document.getElementById(lista).classList.contains("cerrada")) {
+    for (let i = 0; i < listasVideos.length; i++) {
+      listasVideos[i].classList.remove("abierta");
+      listasVideos[i].classList.add("cerrada");
+      document.getElementById(lista).classList.remove("cerrada");
+      document.getElementById(lista).classList.add("abierta");
     }
-    if (document.getElementById(lista).classList.contains("abierta")) {
-        for (let i = 0; i < listasVideos.length; i++) {
-            listasVideos[i].classList.remove("abierta");
-            listasVideos[i].classList.add("cerrada");
-        }
-        return;
+    return;
+  }
+  if (document.getElementById(lista).classList.contains("abierta")) {
+    for (let i = 0; i < listasVideos.length; i++) {
+      listasVideos[i].classList.remove("abierta");
+      listasVideos[i].classList.add("cerrada");
     }
-}
+    return;
+  }
+};
 
 async function cargarLinksDesdeJSON(urlJSON, idContainer) {
-    try {
-        const response = await fetch(urlJSON);
-        const data = await response.json();
-        const linksContainer = document.getElementById(idContainer);
-        if (data.length === 0) {
-            const h1 = document.createElement('h1');
-            h1.textContent = "PRÓXIMAMENTE";
-            linksContainer.appendChild(h1);
-        } else {
-            data.forEach(link => {
-                const anchor = document.createElement('a');
-                anchor.href = link.url;
-                anchor.textContent = link.number;
-                linksContainer.appendChild(anchor);
-            });
-        }
-    } catch (error) {
-        console.error('Error:', error);
+  const jsonURL = new URL(urlJSON, window.location.href);
+  try {
+    const response = await fetch(jsonURL.toString());
+    const data = await response.json();
+    const linksContainer = document.getElementById(idContainer);
+    if (data.length === 0) {
+      const h1 = document.createElement("h1");
+      h1.textContent = "PRÓXIMAMENTE";
+      linksContainer.appendChild(h1);
+    } else {
+      data.forEach((link) => {
+        const anchor = document.createElement("a");
+        anchor.href = link.url;
+        anchor.textContent = link.number;
+        linksContainer.appendChild(anchor);
+      });
     }
-};
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
